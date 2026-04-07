ea92a6c93739ac82e1011c7d1fb2af0c15986a3e
Shubhaan <93507031+Shubhaan@users.noreply.github.com>
2026-04-07 18:17:13 +0530

    Update Firebase API key in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.PRESENCE_IQ_FIREBASE_CONFIG = {
-  apiKey: "",
+  apiKey: "AIzaSyAGO6fC8FM5KCXW499mg0SxeMC5rWRcX7o",
   authDomain: "presenceiq-669a9.firebaseapp.com",
   projectId: "presenceiq-669a9",
   appId: "1:123456789012:web:1234567890123456789012",
